54cb3897be8ed5d2d9e838c72dafa267922c04d3
Geraldine Nitaly Meneses <geraldine.nitaly@gmail.com>
2023-03-23 12:08:29 -0500

    first commit

---
+++ src/main/java/com/gera/microservices/portafolio/msportafolio/MsPortafolioApplication.java
@@ -0,0 +1,13 @@
+package com.gera.microservices.portafolio.msportafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPortafolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPortafolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gera/microservices/portafolio/msportafolio/MsPortafolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gera.microservices.portafolio.msportafolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPortafolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
